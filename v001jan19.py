217fbe6d17036668fb4503e4c9391db178c78e6d
CavywavAviation <36647301+CavywavAviation@users.noreply.github.com>
2019-01-12 19:15:58 +0000

    Update v001jan19.py

--- v001jan19.py
+++ v001jan19.py
@@ -1,3 +1,8 @@
-user_answer=input("Ask me a question.")
-if user_answer == "01041":
+user_answer=input("Ask me a question. ")
+if user_answer == "0":
+    print("0")
+if user_answer == "name":
+    print("I'm a robot. I don't have a name.")
+elif user_answer == "01041":
     print("TEST CODE ACTIVATED")
+    
